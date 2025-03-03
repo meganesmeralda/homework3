fd3a8d3691064002e6549c577fc49966ee3201e8
meganesmeralda <megan.zheng@emory.edu>
2025-03-03 10:11:51 -0500

    edits to sub1

--- submission1/data-code/TaxBurden_Data.R
+++ submission1/data-code/TaxBurden_Data.R
@@ -1,4 +1,15 @@
+# Meta --------------------------------------------------------------------
 
+## Title:         CDC Tax Burden on Tobacco
+## Author:        Megan Zheng
+## Date Created:  3/2/2025
+## Date Edited:   3/2/2025
+## Description:   Data cleaning of CDC data 
+
+
+# Preliminaries -----------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata)
 
 cig.data <- read_csv("data/input/CDC_1970-2018.csv", col_names = TRUE)
 cpi.data <- read_xlsx("data/input/CPI_1913_2019.xlsx", skip = 11)
